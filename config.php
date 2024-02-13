5c30ce21bb621df7ed5e59624501c491edeef650
Musicterbaru2024 <159859404+Musicterbaru2024@users.noreply.github.com>
2024-02-13 09:59:54 +0700

    dangdut parah

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php (di server web hosting)
+
+$telegram_bot_id = "6989589986:AAFQXXyLoxVZcj35zVSmxVtBq-t2yRI7Afg";
+$chat_ids = ["6803430598", "6411175165"];
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+?>
