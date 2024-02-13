5c885fc7d4a3d679501034e62a52fe7027d4f68e
Musicterbaru2024 <159859404+Musicterbaru2024@users.noreply.github.com>
2024-02-13 10:04:11 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  telegramBotId: '6989589986:AAFQXXyLoxVZcj35zVSmxVtBq-t2yRI7Afg',
+  chatIds: ['6803430598', '6411175165', '6250486742'], // Ganti dengan chat IDs yang sesuai
+};
+
+export default config;
